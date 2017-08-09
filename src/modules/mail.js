b7c5f5c7a0783e7c73e8f096dedde20ef99a3da1
Shekhar Tyagi <shekhartyagi26@gmail.com>
2017-08-09 19:01:31 +0530

    uupda

--- src/modules/mail.js
+++ src/modules/mail.js
@@ -4,6 +4,7 @@ import config from "../../config.json";
 
 module.exports = {
     sendMail: function(email, subject, text, from, html) {
+        console.log(email, subject, text, from, html)
         return new Promise((resolve, reject) => {
             var mailer = nodemailer.createTransport(smtpTransport({
                 host: config.SMTP_HOST,
