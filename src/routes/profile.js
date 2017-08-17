33963c1d6e654cda3e419c50d6ba9a8169b0e8ba
Shekhar Tyagi <shekhartyagi26@gail.com>
2017-08-17 12:46:30 +0530

    update

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,8 +4,10 @@ import auth from "../middleware/auth";
 export default (app) => {
 
     /* Route for get profile */
-    app.route("/profile/:id").get(auth.requiresLogin, profile.get);
+    app.route("/profile").get(auth.requiresLogin, profile.get);
 
+    /* Route for edit profile */
+    // app.route("/profile").post(auth.requiresLogin, profile.post);
 
     return app;
 };
